351e19fdbf8a904408590f22901dfb36b00a7d09
aamnnr <noeramin354@gmail.com>
2025-10-22 10:44:35 +0700

    update link api client

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173', // ubah nanti setelah deploy
+  origin: 'login-auth-app-javis.up.railway.app',
   credentials: true
 }));
 app.use(express.json());
@@ -21,13 +21,11 @@ app.use(cookieParser());
 // API routes
 app.use('/api/auth', authRoutes);
 
-// Static file React
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const clientPath = path.join(__dirname, '../client/dist');
 app.use(express.static(clientPath));
 
-// Untuk semua route lainnya, kirim index.html
 app.get('*', (req, res) => {
   res.sendFile(path.join(clientPath, 'index.html'));
 });
