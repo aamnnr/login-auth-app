fc2b55c8d902c4220ca0d4ee338d0dd1e124178e
aamnnr <noeramin354@gmail.com>
2025-10-22 11:07:55 +0700

    update api public

--- client/src/api.js
+++ client/src/api.js
@@ -2,9 +2,8 @@
 
 import axios from 'axios';
 
-// Buat instance axios
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', 
+  baseURL: 'https://login-auth-app-javis.up.railway.app/api', 
   withCredentials: true,
 });
 
--- server/index.js
+++ server/index.js
@@ -12,7 +12,10 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: 'login-auth-app-javis.up.railway.app',
+  origin: [
+    'login-auth-app-javis.up.railway.app',
+    'http://localhost:5173'
+  ],
   credentials: true
 }));
 app.use(express.json());
