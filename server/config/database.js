96559037fe64b975f1a06c07730a53cb63e8abb7
aamnnr <noeramin354@gmail.com>
2025-10-22 11:50:32 +0700

    update db

--- server/config/database.js
+++ server/config/database.js
@@ -4,11 +4,11 @@ import mysql from 'mysql2/promise';
 import 'dotenv/config';
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || mysql.railway.internal,
-  user: process.env.DB_USER || root,
-  password: process.env.DB_PASSWORD || javis,
-  database: process.env.DB_NAME || javis_auth,
-  port: process.env.DB_PORT || 3306,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
