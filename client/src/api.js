66e3411471603fe35ba5ce2c9be50dfecdecbd4b
aamnnr <noeramin354@gmail.com>
2025-11-03 19:28:24 +0700

    update link railway

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'http://login-auth-app-javis.up.railway.app/api',
   withCredentials: true,
 });
 
